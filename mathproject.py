f5431921c156a2f3dc5134a998b84ea9127a21bc
danvluewubley <119687718+danvluewubley@users.noreply.github.com>
2022-12-27 16:47:27 -0500

    puts information in a .txt file

--- mathproject.py
+++ mathproject.py
@@ -25,6 +25,7 @@ def coordinate_handle(x):
       '1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '10': 9, '11': 10, '12': 0, '13': 1, '14': 2, '15': 3, '16': 4, '17': 5, '18': 6, '19': 7, '20': 8, '21': 9, '22': 10, '23': 0, '24': 1, '25': 2, '26': 3
   }.get(str(x))
 
+
 def original_coordinates(name1, num):
   join = ""
   matrix = np.arange(0,len(name1)).reshape((len(name1)//2, 2))
@@ -43,6 +44,7 @@ def original_coordinates(name1, num):
   print(matrix)
   return matrix, table_of_coordinates
 
+
 def reflection_y_coordinates(name1, matrix, table_of_coordinates, num):
   join = ""
   for x in range(len(name1)//2):
@@ -51,9 +53,11 @@ def reflection_y_coordinates(name1, matrix, table_of_coordinates, num):
 
   print("Coordinates after a reflection over the y-axis:")
   print(matrix)
-  with open("info.txt", "w") as f:
+  with open("info.txt", "a") as f:
     f.write(join.join(name[num]) + "'s Coordinates after a reflection over the y-axis\n")
-    f.write(np.array_str(matrix))
+    f.write(np.array_str(matrix) + '\n')
+  f.close()
+
 
 def reflection_x_coordinates(name1, matrix, table_of_coordinates, num):
   for x in range(len(name1)//2):
@@ -63,6 +67,12 @@ def reflection_x_coordinates(name1, matrix, table_of_coordinates, num):
   print("Coordinates after a reflection over the x-axis: ")
   print(matrix)
 
+  with open("info.txt", "a") as f:
+    f.write(join.join(name[num]) + "'s Coordinates after a reflection over the x-axis\n")
+    f.write(np.array_str(matrix) + '\n')
+  f.close()
+
+
 def reflection_origin_coordinates(name1, matrix, table_of_coordinates, num):
   for x in range(len(name1)//2):
     matrix[x, 0] = table_of_coordinates[x] * -1
@@ -71,6 +81,14 @@ def reflection_origin_coordinates(name1, matrix, table_of_coordinates, num):
   print("Coordinates after a reflection over the origin: ")
   print(matrix)
 
+  with open("info.txt", "a") as f:
+    f.write(join.join(name[num]) + "'s Coordinates after a reflection over the origin\n")
+    f.write(np.array_str(matrix) + '\n')
+  f.close()
+
+f = open("info.txt", "r+") 
+f.seek(0) 
+f.truncate() 
 for x in range(len(edited_name)):
   matrix, table_of_coordinates = original_coordinates(edited_name[x], num)
   reflection_y_coordinates(edited_name[x], matrix, table_of_coordinates, num)
